991fccc4a170b06183515cd30d93206684d1be82
Carlos Henrique <carloshenriquesantanas19@gmail.com>
2024-07-26 00:30:17 -0300

    chore: add testing setup

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  verbose: true,
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
+
+export default createJestConfig(config)
